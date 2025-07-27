d8b5f89d9b307ecb5f178265c1a9089f9f516da5
ramshideranhikal <42137887+ramshideranhikal@users.noreply.github.com>
2025-07-27 17:38:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace with your repository name
+  base: 'ramshideranhikal/ramshideranhikal.github.io', // Replace with your repository name
   build: {
     outDir: 'dist',
     sourcemap: true
